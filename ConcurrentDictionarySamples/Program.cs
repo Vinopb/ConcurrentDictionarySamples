3c799e4571b66695c39be2d96b919186f5dff0bd
Vinod - PC <Vinod - PC@VINOD>
2016-11-17 09:10:03 +0530

    GetOrAdd and thread safety.

--- ConcurrentDictionarySamples/Program.cs
+++ ConcurrentDictionarySamples/Program.cs
@@ -3,24 +3,41 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConcurrentDictionarySamples
 {
     class Program
     {
+        private static int runCount = 0;
+        private static readonly ConcurrentDictionary<string, string> dict = 
+            new ConcurrentDictionary<string, string>();
+
         static void Main(string[] args)
         {
-            var dict = new ConcurrentDictionary<string, string>();
-
-           var value=  dict.GetOrAdd("key", x => "The First Value");
-            Console.WriteLine(value);
-
-            value = dict.GetOrAdd("key", x => "The Secnd Value");
-            Console.WriteLine(value);
 
 
-           
+            var task1 = Task.Run(() => PrintValue("The Firs Value"));
+            var task2 = Task.Run(() => PrintValue("The Second Value"));
+
+            Task.WaitAll(task1, task2);
+
+            PrintValue("The Third Value");
+            Console.WriteLine($"Run Count :{runCount}");
+            Console.ReadLine();
+        }
+
+        private static void PrintValue(string valueToPrint)
+        {
+           var valueToFound = dict.GetOrAdd("key",
+                x =>
+                 {
+                     Interlocked.Increment(ref runCount);
+                     Thread.Sleep(100);
+                     return valueToPrint;
+                 });
+            Console.WriteLine(valueToFound);
         }
 
        
